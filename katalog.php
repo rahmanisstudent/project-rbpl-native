a92a96bb22fec8c7e619152a4c6f6ff1afa41577
Muhamad Nobel Wurjayatma <124230114@student.upnyk.ac.id>
2025-06-10 22:12:27 +0700

    seadanya

---
+++ katalog.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Gambar</title>
+    <link rel="stylesheet" href="css/katalog.css">
+</head>
+<body>
+    <?php include 'nav.php'; ?>
+
+    <div class="container">
+        <h1>Daftar Model</h1>
+        <div class="item">
+            <img src="gambar1.jpg" alt="Gambar 1">
+        </div>
+        <div class="item">
+            <img src="gambar2.jpg" alt="Gambar 2">
+        </div>
+        <div class="item">
+            <img src="gambar3.jpg" alt="Gambar 3">
+        </div>
+    </div>
+</body>
+</html>
